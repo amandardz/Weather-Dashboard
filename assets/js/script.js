c8823e871ff04bc67fd7639f44677a6d77983135
Amanda Rodriguez <amandardz15@gmail.com>
2021-03-19 18:18:38 -0500

    modifying javascript and index

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var cityFormEl = document.querySelector('#cityForm')
 var searchBtnEl = document.querySelector('#searchBtn')
 var cityNameEl = document.querySelector('#cityName')
 var cityListEl = document.querySelector('#cityList')
+var weeklyEl = document.querySelector('#weeklyWeather')
+var dailyCardEl = document.querySelector('.daily')
 var currentDate = new Date().toLocaleDateString('en-US')
 
 var printCityList = function(name) {
@@ -21,12 +23,12 @@ var getCurrentWeather = function (cityName) {
         .then(function (data) {
             console.log(data)
 
-            var headingEl = document.getElementById('heading1');
-            headingEl.textContent = data.name + " " + "(" + currentDate + ")";
+            var headingEl = document.querySelector('#heading1');
+            headingEl.textContent = data.name + ' ' + '(' + currentDate + ')';
             console.log(data.name)
 
             var temp1El = document.querySelector('#temp1')
-            temp1El.textContent = "Temperature: " + data.main.temp + " ℉"
+            temp1El.textContent = "Temperature: " + Math.round(data.main.temp) + " ℉"
 
             var humid1El = document.querySelector('#humid1')
             humid1El.textContent = "Humid: " + data.main.humidity + "%"
@@ -34,7 +36,7 @@ var getCurrentWeather = function (cityName) {
             var wind1El = document.querySelector('#wind1')
             wind1El.textContent = "Wind Speed: " + data.wind.speed + " MPH"
 
-            var uvUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&appid=f05e59dca587993db2e06e2c3a372a11'
+            var uvUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&exclude=current,minutely,hourly,alerts&units=imperial&appid=f05e59dca587993db2e06e2c3a372a11'
 
             fetch(uvUrl)
                 .then(function(response){
@@ -43,36 +45,27 @@ var getCurrentWeather = function (cityName) {
                 .then(function (data){
                     console.log(data)
                     var uv1El = document.querySelector('#uv1')
-                    uv1El.textContent = "UV Index: " + data.value
+                    uv1El.textContent = "UV Index: " + data.daily[0].uvi
         
-                    if(data.value === 0 || data.value < 3){
+                    if(data.daily[0].uvi === 0 || data.daily[0].uvi < 3){
                         uv1El.setAttribute("style", "color: green;")
-                    } else if (data.value >= 3 && data.value <= 6) {
+                    } else if (data.daily[0].uvi >= 3 && data.daily[0].uvi <= 6) {
                         uv1El.setAttribute("style", "color: yellow;")
                     } else {
                         uv1El.setAttribute("style", "color: red;")
                     }
+
+                    for(var i = 1; i < 6; i++){
+                        var dailyDate = new Date(data.daily[i].dt * 1000).toLocaleDateString('en-US')
+                        var weeklyDiv = document.createElement('div')
+                        weeklyEl.appendChild(weeklyDiv)
+                        var p = document.createElement('p')
+                        weeklyDiv.appendChild(p)
+                        p.innerHTML = '<p>' + dailyDate + '</p>' + '<p>' + "Temp: " + Math.round(data.daily[i].temp.max) + " ℉" + '</p>' + '<p>' + "Humid: " + data.daily[i].humidity + "%" + '</p>'
+                    }
                 })
-        });
+        })
 };
-
-
-// var getDailyWeather = function(cityName) {
-//     var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=f05e59dca587993db2e06e2c3a372a11'
-//     console.log(weatherUrl)
-//     fetch(weatherUrl)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data)
-//             for(var i = 0; i < ; i++){
-//                 var dailyCards = document.querySelector('.daily')
-//                 dailyCards.textContent = data.list[i].main.temp
-//             });
-// };
-
-// getDailyWeather();
 
 cityFormEl.addEventListener('submit', function(event){
     event.preventDefault();
@@ -89,4 +82,3 @@ cityFormEl.addEventListener('submit', function(event){
         getCurrentWeather(cityNameVal);
     }
 });
-
