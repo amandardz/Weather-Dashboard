857d98a28a92e28d51a397995314b85ce35f965f
Amanda Rodriguez <amandardz15@gmail.com>
2021-03-19 19:22:36 -0500

    modifying javascript and index

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var cityFormEl = document.querySelector('#cityForm')
 var searchBtnEl = document.querySelector('#searchBtn')
 var cityNameEl = document.querySelector('#cityName')
 var cityListEl = document.querySelector('#cityList')
+var currentWeatherEl = document.querySelector('#currentWeather')
 var weeklyEl = document.querySelector('#weeklyWeather')
 var dailyCardEl = document.querySelector('.daily')
 var currentDate = new Date().toLocaleDateString('en-US')
@@ -22,19 +23,14 @@ var getCurrentWeather = function (cityName) {
         })
         .then(function (data) {
             console.log(data)
+            var iconCode = data.weather[0].icon
+            var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png'
 
-            var headingEl = document.querySelector('#heading1');
-            headingEl.textContent = data.name + ' ' + '(' + currentDate + ')';
-            console.log(data.name)
-
-            var temp1El = document.querySelector('#temp1')
-            temp1El.textContent = "Temperature: " + Math.round(data.main.temp) + " ℉"
-
-            var humid1El = document.querySelector('#humid1')
-            humid1El.textContent = "Humid: " + data.main.humidity + "%"
-
-            var wind1El = document.querySelector('#wind1')
-            wind1El.textContent = "Wind Speed: " + data.wind.speed + " MPH"
+            var currentWeatherDiv = document.createElement('div')
+            currentWeatherEl.appendChild(currentWeatherDiv)
+            var currentWeatherInfo = document.createElement('p')
+            currentWeatherDiv.appendChild(currentWeatherInfo)
+            currentWeatherInfo.innerHTML = '<h2>' + data.name + ' ' + currentDate + ' ' + '<img src="' + iconUrl + '">' + '</h2>' + '<p>' + "Temp: " + Math.round(data.main.temp) + " ℉" + '</p>' + '<p>' + "Humid: " + data.main.humidity + "%" + '</p>' + '<p>' + "Wind Speed: " + data.wind.speed + " MPH" + '</p>'
 
             var uvUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&exclude=current,minutely,hourly,alerts&units=imperial&appid=f05e59dca587993db2e06e2c3a372a11'
 
@@ -44,13 +40,14 @@ var getCurrentWeather = function (cityName) {
                 })
                 .then(function (data){
                     console.log(data)
-                    var uv1El = document.querySelector('#uv1')
+                    var uv1El = document.createElement('div')
+                    currentWeatherDiv.appendChild(uv1El)
                     uv1El.textContent = "UV Index: " + data.daily[0].uvi
         
                     if(data.daily[0].uvi === 0 || data.daily[0].uvi < 3){
                         uv1El.setAttribute("style", "color: green;")
                     } else if (data.daily[0].uvi >= 3 && data.daily[0].uvi <= 6) {
-                        uv1El.setAttribute("style", "color: yellow;")
+                        uv1El.setAttribute("style", "color: orange;")
                     } else {
                         uv1El.setAttribute("style", "color: red;")
                     }
@@ -59,9 +56,9 @@ var getCurrentWeather = function (cityName) {
                         var dailyDate = new Date(data.daily[i].dt * 1000).toLocaleDateString('en-US')
                         var weeklyDiv = document.createElement('div')
                         weeklyEl.appendChild(weeklyDiv)
-                        var p = document.createElement('p')
-                        weeklyDiv.appendChild(p)
-                        p.innerHTML = '<p>' + dailyDate + '</p>' + '<p>' + "Temp: " + Math.round(data.daily[i].temp.max) + " ℉" + '</p>' + '<p>' + "Humid: " + data.daily[i].humidity + "%" + '</p>'
+                        var dailyWeatherInfo = document.createElement('p')
+                        weeklyDiv.appendChild(dailyWeatherInfo)
+                        dailyWeatherInfo.innerHTML = '<p>' + dailyDate + '</p>' + '<p>' + "Temp: " + Math.round(data.daily[i].temp.max) + " ℉" + '</p>' + '<p>' + "Humid: " + data.daily[i].humidity + "%" + '</p>'
                     }
                 })
         })
